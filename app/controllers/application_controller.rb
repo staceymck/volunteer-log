97d61a3826cd437219ee56c780b248da1621293f
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 13:22:42 -0600

    Add authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!@current_user
+  end
+
+  def require_login
+    redirect_to root_path if !logged_in?
+  end
 end
