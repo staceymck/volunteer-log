f9b4a6589a8c99e4a404cb0f392709713650a935
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 15:33:50 -0600

    Add values to enum attributes

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,4 +2,6 @@ class Volunteer < ApplicationRecord
   belongs_to :user
   has_many :activities
   has_many :roles, through: :activities
+  enum age_group: {over_eighteen: 0, over_twenty_one: 1}
+  enum background_check_status: {incomplete: 0, approved: 1, flagged: 2}
 end
