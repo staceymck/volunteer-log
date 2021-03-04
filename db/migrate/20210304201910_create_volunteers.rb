0b9c48fb9a4d4daad771abe3cf41da1426f9dc95
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 15:21:48 -0600

    Add default values

--- db/migrate/20210304201910_create_volunteers.rb
+++ db/migrate/20210304201910_create_volunteers.rb
@@ -3,14 +3,14 @@ class CreateVolunteers < ActiveRecord::Migration[6.0]
     create_table :volunteers do |t|
       t.string :first_name, null: false, index: true
       t.string :last_name, null: false, index: true
-      t.integer :age_group
+      t.integer :age_group, default: 0
       t.string :phone
       t.string :email
       t.date :birthday
       t.string :occupation
       t.string :employer
       t.text :interests
-      t.integer :background_check_status
+      t.integer :background_check_status, default: 0
       t.string :photo
       t.belongs_to :user, null: false, foreign_key: true, index: true
 
