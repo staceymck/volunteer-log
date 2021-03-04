1666425864839bea9da4d971010c562141eb3ef8
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 15:23:24 -0600

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true
   has_many :volunteers
   has_many :roles
   has_many :activities, through: :volunteers
