d4d7469a03dc34e82b3d59895dd51b52309cc34c
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 11:23:39 -0600

    Create omniauth action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  
+  def omniauth
+    user = User.create_from_omniauth(auth)
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = user.errors.full_messages.join(“, “)
+      redirect_to ‘/’
+    end
+ end
+
+  private
+  def auth
+    request.env['omniauth.auth']
+  end
 end
