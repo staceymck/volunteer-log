fba0e7f11fb1c9e9c3d2a38247152acc9fad555e
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 08:43:36 -0600

    Add provider and database level uniqueness check

--- db/migrate/20210303202224_create_users.rb
+++ db/migrate/20210303202224_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, null: false, index: {unique: true}
       t.string :email
       t.string :password_digest
-      t.integer :uid
+      t.string :uid
+      t.string :provider
 
       t.timestamps
     end
