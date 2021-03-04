2dbc7ccdaffdc4dbaac4e7bd3c55377651b8c663
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 12:30:48 -0600

    Set up routes for login/signup/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+  root 'sessions#home'
+  
+  #Users & Sessions
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   get '/auth/:provider/callback', to: 'sessions#omniauth'
+  
+  resources :users, only: [:show, :index, :delete]
 end
