36599872ac131284d41c4483535c9c8e05204049
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 14:47:28 -0600

    Generate roles as resource

---
+++ db/migrate/20210304203215_create_roles.rb
@@ -0,0 +1,16 @@
+class CreateRoles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :roles do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :title, null: false, index: true
+      t.text :description
+      t.integer :age_requirement, default: 0
+      t.integer :frequency, default: 0
+      t.string :days
+      t.boolean :background_check_required?, default: 0
+      t.integer :status, default: 0, index: true
+
+      t.timestamps
+    end
+  end
+end
