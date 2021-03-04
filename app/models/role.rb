6ad1573166bb39e094ab4306a0de63f29b9eb30a
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 15:33:59 -0600

    Add values to enum attributes

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,8 @@ class Role < ApplicationRecord
   belongs_to :user
   has_many :activities
   has_many :volunteers, through: :activities
+  enum age_requirement: {over_eighteen: 0, over_twenty_one: 1, chaperone_under_eighteen: 2}
+  enum frequency: {ongoing: 0, one_time: 1}
+  enum status: {recruiting: 0, not_recruiting: 1}
+
 end
