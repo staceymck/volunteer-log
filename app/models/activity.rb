88dab0b441441fddd53dfbab4a89c804d7014561
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-05 15:00:59 -0600

    Add scopes to group by user and order by date

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,12 @@
 class Activity < ApplicationRecord
   belongs_to :volunteer
   belongs_to :role
+  scope :user_set, -> (user) {joins(:volunteer).where(volunteers: {user_id: user.id})}
+  scope :newest, -> {order(date: :desc)}
+  scope :oldest, -> {order(:date)}
+
+  # def self.user_set(user)
+  #   joins(:volunteer).where(volunteers: {user_id: user.id})
+  # end
+
 end
