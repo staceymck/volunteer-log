1837b82f71b0392f8666ae9125790eb4781ea22a
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-05 11:26:48 -0600

    Begin building out methods for dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,49 @@ class User < ApplicationRecord
       u.password = SecureRandom.hex(16)
     end
   end
+
+  #Cummulative totals
+  def total_hours
+    activities.sum(:duration)
+  end
+
+  def total_activities
+    activities.count
+  end
+
+  def unique_volunteers
+    volunteers.uniq.count
+  end
+
+  #Monthly totals
+  def hours_this_month
+    activities.where("cast(strftime('%m', date) as int) = ?", Date.today.month).sum(:duration)
+  end
+
+  def activities_this_month
+    activities.where("cast(strftime('%m', date) as int) = ?", Date.today.month).count
+  end
+
+  # def unique_volunteers_this_month
+  #   activities.where("cast(strftime('%m', date) as int) = ?", Date.today.month).group('volunteer_id')
+  # end
+
+  # def new_volunteers_this_month
+  #   activities.where("cast(strftime('%m', date) as int) = ?", Date.today.month).group('volunteer_id')
+  # end
+
+  #Yearly totals
+  def hours_this_year
+    activities.where("cast(strftime('%Y', date) as int) = ?", Date.today.year).sum(:duration)
+  end
+
+  def activities_this_year
+    activities.where("cast(strftime('%Y', date) as int) = ?", Date.today.year).count
+  end
+
+  # def unique_volunteers_this_year
+  # end
+
+  # def new_volunteers_this_year
+  # end
 end
