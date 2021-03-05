aab394f16bd7a92a32e0bfa6409406c3ed2c2439
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 19:00:08 -0600

    Set up actions for making new volunteers

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,2 +1,19 @@
 class VolunteersController < ApplicationController
+  def new
+    @volunteer = Volunteer.new
+  end
+
+  def create
+    @volunteer = current_user.volunteers.build(volunteer_params)
+    if @volunteer.save
+      redirect_to volunteer_path(@volunteer)
+    else
+      render :new
+    end
+  end
+
+  private
+  def volunteer_params
+    params.require(:volunteer).permit(:first_name, :last_name, :email, :phone, :occupation, :employer, :birthday, :age_group, :background_check_status, :photo_link, :interests)
+  end
 end
