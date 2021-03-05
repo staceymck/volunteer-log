8739b2e32320933075a50cd059d2f95724a47a5e
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-04 18:48:21 -0600

    Add group for those under 18

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,6 +2,6 @@ class Volunteer < ApplicationRecord
   belongs_to :user
   has_many :activities
   has_many :roles, through: :activities
-  enum age_group: {over_eighteen: 0, over_twenty_one: 1}
+  enum age_group: {over_eighteen: 0, over_twenty_one: 1, under_eighteen: 2}
   enum background_check_status: {incomplete: 0, approved: 1, flagged: 2}
 end
