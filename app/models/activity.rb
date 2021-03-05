8dfa9590d20d5059bd51e883275ef4e8c9c23137
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-05 15:44:52 -0600

    Build out query method

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,4 +9,33 @@ class Activity < ApplicationRecord
   #   joins(:volunteer).where(volunteers: {user_id: user.id})
   # end
 
+  def self.apply_query(query)
+    if query.present?
+      if query == "oldest"
+        oldest
+      elsif query == "newest"
+        newest
+      else
+        where("first_name LIKE ?", "%#{query}%")  #need to make it searchable by first and/or last name
+      end
+    else
+      newest
+    end
+  end
+
+  #Alternative if diving out search vs filter as 2 different types of queries
+  # def self.filter_results(search, chosen_filter)
+  #  if search.present?
+  #   where("first_name LIKE ?", "%#{search}%")
+  #  elsif chosen_filter.present?
+  #   if chosen_filter == "oldest"
+  #     oldest
+  #   elsif chosen_filter == "newest"
+  #     newest
+  #   end
+  #  else
+  #    newest
+  #  end
+  # end
+
 end
