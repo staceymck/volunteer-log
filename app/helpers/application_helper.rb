d6d2ab6cddc7c22e2d12e124b470ccaa7235f094
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-05 12:13:19 -0600

    Add method to display booleans as Yes/No

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def display_hours(hours)
     sprintf("%g", hours)
   end
+
+  def display_yes_no(boolean_attribute)
+    boolean_attribute == 0 ? "No" : "Yes"
+  end
 end
