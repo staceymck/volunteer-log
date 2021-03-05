48ccad686e74995768e0b439a23eeb5b30cb217e
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-05 12:11:18 -0600

    Build all routes for roles controller

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,55 @@
 class RolesController < ApplicationController
+  before_action :find_role, :redirect_if_not_authorized, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @roles = current_user.roles.all
+  end
+  
+  def show
+  end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = current_user.roles.build(role_params)
+    if @role.save
+      redirect_to role_path(@role)
+    else
+      render :new #make sure to add field_with_errors set up
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @role.update(role_params)
+      redirect_to role_path(@role)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @role.destroy
+    redirect_to '/'
+  end
+
+  private
+  def role_params
+    params.require(:role).permit(:title, :description, :age_requirement, :frequency, :days, :background_check_required, :status)
+  end
+
+  def find_role
+    @role = Role.find(params[:id])
+  end
+
+  def redirect_if_not_authorized
+    if @role.user != current_user
+      flash[:alert] = "Invalid record"
+      redirect_to '/'
+    end
+  end
 end
