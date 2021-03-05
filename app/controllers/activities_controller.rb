08a78f79fb548e123cd2c12deee602b0eddd16fa
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-05 14:59:55 -0600

    Build out RESTful actions

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,55 @@
 class ActivitiesController < ApplicationController
+  before_action :find_activity, :redirect_if_not_authorized, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @activities = Activity.user_set(current_user) #can add either .oldest or .newest to order
+  end
+
+  def show
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = current_user.activities.build(activity_params)
+    if @activity.save
+      redirect_to activity_path(@activity)
+    else
+      render :new #fields with errors
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @activity.update(activity_params)
+      redirect_to activity_path(@activity)
+    else
+      render :edit #fields with errors
+    end
+  end
+
+  def destroy
+    @activity.destroy
+    redirect_to '/'
+  end
+
+  private
+  def activity_params
+    params.require(:activity).permit(:date, :duration, :volunteer_id, :role_id)
+  end
+
+  def find_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def redirect_if_not_authorized
+    if @activity.volunteer.user != current_user
+      flash[:alert] = "Invalid record"
+      redirect_to '/'
+    end
+  end
 end
