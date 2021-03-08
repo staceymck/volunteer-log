15f49ba12625962fa975f6ff109ff2daa2570501
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-08 14:08:28 -0600

    Add method to display phone

--- app/helpers/volunteers_helper.rb
+++ app/helpers/volunteers_helper.rb
@@ -3,4 +3,8 @@ module VolunteersHelper
   def display_age_group(volunteer)  
     volunteer.age_group.gsub('_', " ").gsub("twenty one", "21").gsub("eighteen", "18").capitalize
   end
+
+  def display_phone(phone)
+    phone.gsub(".", "-")
+  end
 end
