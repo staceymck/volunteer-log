fbbe6a2f20687c7bf709bc527b7ecd2f63f47380
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-10 14:13:43 -0600

    Add message for users with no records (ie new user instructions)

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,10 @@
 module DashboardHelper
+  
+  def new_user_welcome
+    if @user.volunteers.size == 0 && @user.roles.size == 0
+    content_tag(:div, content_tag(:p, "This dashboard will display stats as you add volunteers
+     roles and activity records.") + content_tag(:p, "To get started, add a volunteer and a role.
+     Then start tracking each volunteer engagement as an activity record."), class: "welcome")
+    end
+  end
 end
