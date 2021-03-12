0ce36f09dfa5882f993feafbb68bbde29cb78930
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-12 09:24:05 -0600

    Remove instance variable use

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,7 +1,7 @@
 module DashboardHelper
   
   def new_user_welcome
-    if @user.volunteers.size == 0 && @user.roles.size == 0
+    if current_user.volunteers.size == 0 && current_user.roles.size == 0
     content_tag(:div, content_tag(:p, "This dashboard will display stats as you add volunteers
      roles and activity records.") + content_tag(:p, "To get started, add a volunteer and a role.
      Then start tracking each volunteer engagement as an activity record."), class: "welcome")
