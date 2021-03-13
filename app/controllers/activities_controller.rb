2d8a42af6dd6455505a862320728660db8f25437
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-13 17:12:38 -0600

    Add check to ensure volunteer and/or role belongs to user

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,11 @@
 class ActivitiesController < ApplicationController
   before_action :find_activity, :redirect_if_not_authorized, only: [:show, :edit, :update, :destroy]
+  before_action :validate_role_and_volunteer_belong_to_user, only: [:create, :update]
   
   def index
     flash.now[:search_guide] = "Please enter a name" if params[:query] == ""
 
-    #handle nested route /volunteers/:volunteer_id/activities
-    if params[:volunteer_id] && @volunteer = current_user.volunteers.find_by(id: params[:volunteer_id])
+    if params[:volunteer_id] && @volunteer = current_user.volunteers.find_by(id: params[:volunteer_id]) #handle nested route /volunteers/:volunteer_id/activities
       @activities = @volunteer.activities.apply_query(params[:query])
     else
       flash.now[:alert] = "Volunteer not found" if params[:volunteer_id]
@@ -16,7 +16,7 @@ class ActivitiesController < ApplicationController
   def show
   end
 
-  def new #redirects to activity page if your save is unsuccessful and you press refresh - why?
+  def new
     if params[:volunteer_id] && @volunteer = current_user.volunteers.find_by(id: params[:volunteer_id])
       @activity = @volunteer.activities.build
     else
@@ -75,6 +75,15 @@ class ActivitiesController < ApplicationController
     if @activity.volunteer.user != current_user
       flash[:alert] = "Invalid record"
       redirect_to dashboard_path
+    end
+  end
+
+  def validate_role_and_volunteer_belong_to_user
+    role_id = params[:activity][:role_id]
+    volunteer_id = params[:volunteer_id] || params[:activity][:volunteer_id]
+    if (role_id != "" && current_user.roles.find_by(id: role_id).nil?) || (volunteer_id != "" && current_user.volunteers.find_by(id: volunteer_id).nil?)
+      flash[:alert] = "You can only save/alter activity records for volunteers and roles you've created"
+      redirect_to new_activity_path
     end
   end
 
